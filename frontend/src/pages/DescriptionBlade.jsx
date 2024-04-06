5eb2f395267725353ef24de21f80b4830fcd6040
Thibault Zhou <thetibo1@gmail.com>
2024-04-06 20:55:34 +0200

    small update

--- frontend/src/pages/DescriptionBlade.jsx
+++ frontend/src/pages/DescriptionBlade.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 
 function DescriptionBlade() {
-  return <div>DescriptionBlade {}</div>;
+  return (
+    <div>
+      <h2>Ex</h2>
+      {/* <img></img> */}
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+        corrupti ullam ab. Reiciendis voluptatum laudantium consectetur, facere
+        distinctio libero molestias incidunt sit dolor. Deserunt illo laudantium
+        dolores aut cumque doloribus.
+      </p>
+    </div>
+  );
 }
 
 export default DescriptionBlade;
 
-//  delete ficher peut etre
+//  mettre id du jeu ici
